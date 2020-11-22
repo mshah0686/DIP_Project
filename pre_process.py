9edbda7cc3ab80a687929f8d2f433539923a43c9
mshah0686 <mshah0686@gmail.com>
2020-11-22 00:06:44 -0600

    More outlining

--- pre_process.py
+++ pre_process.py
@@ -1,6 +1,22 @@
+import cv2 as cv
 '''
-Image preprocessing methods
+Take a soduku image, and return a clean mask after processing
+'''
 
-Histogram equalization, thresholding, filterng...
-'''
+def histogram_equalization(img):
+    return cv.equalizeHist(img)
+
+def dilate(img, kernel, iterations=1):
+    return cv2.dilate(img,kernel,iterations = 1)
+
+def erosion(img, kernel, iterations=1):
+    return cv2.erode(img, kernel, iterations)
+
+def threshold(low = 127, upper=255):
+    return cv.threshold(img,low,upper,cv.THRESH_BINARY)
+
+# TODO: use this for thresholding: OTSU's binarazation: https://docs.opencv.org/master/d7/d4d/tutorial_py_thresholding.html
+
+def create_mask(img):
+    #TODO: Main method for filtering and masking
 

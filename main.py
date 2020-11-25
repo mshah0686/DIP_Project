5374b397cbfda1dce1428c7eb58bca2bee78e622
mshah0686 <mshah0686@gmail.com>
2020-11-25 12:28:19 -0600

    A better predicting model with todo notes

--- main.py
+++ main.py
@@ -15,17 +15,17 @@ Main file:
 
 def localizeEquation(img):
     contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    unprocessed_digits = []
+    expected_digits = []
+    expected_digits_x_loc = []
 
     overlay_image = img.copy()
     overlay_image = cv2.cvtColor(overlay_image, cv2.COLOR_BGR2RGB)
     for c in contours:
         area = cv2.contourArea(c)
-        if(area > 50):
+        if(area > 30):
             (x,y,w,h) = cv2.boundingRect(c)
             cv2.rectangle(overlay_image, (x,y), (x+w, y+h), (255, 0, 0), 2)
             sub_image = img[y:y+h, x:x+w]
-            #cv2.imshow("Area of interest", sub_image)
             max_dimen = max(sub_image.shape[0], sub_image.shape[1]) + 10
             x_pad = ((max_dimen - sub_image.shape[0]) // 2)
             y_pad = ((max_dimen - sub_image.shape[1]) // 2) 
@@ -33,15 +33,22 @@ def localizeEquation(img):
             sub_image = cv2.resize(sub_image, (28, 28))
             cv2.imshow("Area of interest", sub_image)
             cv2.waitKey(0)
-            unprocessed_digits.append(sub_image)
+            expected_digits.append(sub_image)
+            expected_digits_x_loc.append(x)
     cv2.imshow("Rectangle", overlay_image)
     cv2.waitKey(0)
-    return unprocessed_digits
+
+    #order the digits based on x axis: know ordering for equation
+    X = np.array(expected_digits)
+    Y = np.array(expected_digits_x_loc)
+    inds = Y.argsort()
+    sorted_list = X[inds]
+    return sorted_list
 
 if __name__ == "__main__":
     print("Reading Image")
-    img = cv2.imread('sample.jpg',0)
-    img = imutils.resize(img, height = 200, width=200)
+    img = cv2.imread('sample3.jpg',0)
+    img = imutils.resize(img, height = 400, width=400)
     print("Doing pre processing")
     img = pre_process.process_pipeline(img)
     areas_of_interest = np.asarray(localizeEquation(img))
@@ -51,7 +58,6 @@ if __name__ == "__main__":
     #Machine Learning Part
     print("Starting Machine Learning Part")
     #Reshape data
-    print(areas_of_interest)
     x_test = areas_of_interest.reshape(areas_of_interest.shape[0], 28, 28, 1)
     x_test = x_test.astype('float32')
     x_test /= 255
@@ -64,8 +70,10 @@ if __name__ == "__main__":
     print("Running prediction")
     predictions = digit_model.predict(x_test)
     print("Predictions: ", predictions)
+    characters = []
     for p in predictions:
         max_index = np.argmax(p)
         max_val = np.max(p)
-        if(max_val * 100 > 50):
-            print("Predicted: ", max_index)
+        if(max_val > 0.50):
+            characters.append(max_index)
+    print(characters)
--- pre_process.py
+++ pre_process.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import imutils
 '''
 Take a soduku image, and return a clean mask after processing
 '''
@@ -22,26 +23,76 @@ Gaussian Blur: cv2.GaussianBlur(img,(5,5),0)
 '''
 
 def process_pipeline(img):
+    #Final process pipeline, used in main.py
+    img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+    img = cv2.bitwise_not(img)
+    return img
+
+def testing_pipeline(img):
+    '''
+    Use this for testing different filters
+    '''
     #ret, img = cv2.threshold(img,90,255,cv2.THRESH_BINARY)
     #img = cv2.GaussianBlur(img,(3,3),0)
     img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+    cv2.imshow("threshold", img)
+    cv2.waitKey(0)
     kernel = np.ones((2,2),np.uint8)
-    #img = cv2.dilate(img,kernel,iterations = 1)
+    img = cv2.dilate(img,kernel,iterations = 1)
+    cv2.imshow("erode", img)
+    cv2.waitKey(0)
     img = cv2.erode(img,kernel,iterations = 1)
+    cv2.imshow("erode", img)
+    cv2.waitKey(0)
     #img = cv2.dilate(img,kernel,iterations = 1)
     #ret, img = cv2.threshold(img,230,255,cv2.THRESH_BINARY)
     img = cv2.bitwise_not(img)
-    img = cv2.bilateralFilter(img,9,20,75)
+    cv2.imshow("erode", img)
+    cv2.waitKey(0)
+    #img = cv2.bilateralFilter(img,9,20,75)
+    #cv2.imshow("erode", img)
+    #cv2.waitKey(0)
     #img = cv2.medianBlur(img,3)
     #img = cv2.dilate(img,kernel,iterations = 1)
     return img
 
+def smart_processing(img):
+    '''
+    Look at areas of interest, and remove all noise not in those areas
+    I realize that this is really stupid actually, you're just finding contours twice..
+    '''
+    img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+    img = cv2.bitwise_not(img)
+    cv2.imshow("threshold", img)
+    cv2.waitKey(0)
+    
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    overlay_image = img.copy() * 0
+    #overlay_image = cv2.cvtColor(overlay_image, cv2.COLOR_BGR2RGB)
+    print("Total contours found: ", len(contours))
+    for c in contours:
+        area = cv2.contourArea(c)
+        if(area > 10):
+            (x,y,w,h) = cv2.boundingRect(c)
+            overlay_image[y:y+h, x:x+w] = img[y:y+h, x:x+w]
+            cv2.rectangle(overlay_image, (x,y), (x+w, y+h), (255, 0, 0), 2)
+            sub_image = img[y:y+h, x:x+w]
+            cv2.imshow("Area of interest", sub_image)
+            cv2.waitKey(0)
+    cv2.imshow("Rectangle", overlay_image)
+    cv2.waitKey(0)
+
 if __name__ == "__main__":
+    '''
+    Will run testing filter pipeline
+    '''
     print("Running main function")
-    img = cv2.imread('sudoku_sample.jpg',0)
-    processed = process_pipeline(img)
-    cv2.imshow('original image',img)
-    cv2.imshow('processed image', processed)
+    img = cv2.imread('sample3.jpg',0)
+    img = imutils.resize(img, height = 400, width=400)
+    #smart_processing(img)
+    #processed = testing_pipeline(img)
+    #cv2.imshow('original image',img)
+    #cv2.imshow('processed image', processed)
 
     k = cv2.waitKey(0)
     while(k != 27):
